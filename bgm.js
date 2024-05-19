0690769f45c20cc9d39afa02c3cb717d17edf5ae
sienna0721 <lnai45832@gmail.com>
2024-05-19 18:10:45 +0800

    hell

---
+++ bgm.js
@@ -0,0 +1,9 @@
+function start(){
+let splash= document.getElementById('splash');
+
+splash.addEventListener('transitionend',() => {
+document.getElementById('bgm').play();
+splash.remove();
+});
+splash.classList.add('hide');
+}
