1913149f83e24157f771d84464fdca52252b0cd1
sienna0721 <lnai45832@gmail.com>
2024-05-20 15:30:15 +0800

    Update bgm.js

--- bgm.js
+++ bgm.js
@@ -7,10 +7,3 @@ splash.remove();
 });
 splash.classList.add('hide');
 }
-var interval = setInterval(function(){
-          var countForVideo = document.getElementById('vid').readyState;
-          if(countForVideo == 4){
-            document.getElementById('vid').play();
-            clearInterval(interval);
-          }
-        },2000);
